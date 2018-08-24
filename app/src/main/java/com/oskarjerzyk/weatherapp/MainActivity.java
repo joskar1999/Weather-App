c51849128e6bb7e55ab34406eec8621c9a284317
joskar1999 <joskar1999@gmail.com>
2018-08-24 14:43:24 +0200

    Initial commit
    MainActivity created using Tablayout, tabs set up properly

---
+++ app/src/main/java/com/oskarjerzyk/weatherapp/MainActivity.java
@@ -0,0 +1,76 @@
+package com.oskarjerzyk.weatherapp;
+
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter sectionsPagerAdapter;
+
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(sectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    NowFragment nowFragment = new NowFragment();
+                    return nowFragment;
+                case 1:
+                    ForecastFragment forecastFragment = new ForecastFragment();
+                    return forecastFragment;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            switch (position){
+                case 0:
+                    return "NOW";
+                case 1:
+                    return "FORECAST";
+            }
+            return null;
+        }
+    }
+}
