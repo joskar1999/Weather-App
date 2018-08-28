ccb64ea43513dbcbb5c8165420e83fecfc5fa913
joskar1999 <joskar1999@gmail.com>
2018-08-28 21:46:12 +0200

    Polish language version added

--- app/src/main/java/com/oskarjerzyk/weatherapp/NowFragment.java
+++ app/src/main/java/com/oskarjerzyk/weatherapp/NowFragment.java
@@ -3,7 +3,6 @@ package com.oskarjerzyk.weatherapp;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
