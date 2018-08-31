1b2694b61ade9bb7876bd16dec0a8cde68391b9d
joskar1999 <joskar1999@gmail.com>
2018-08-31 13:42:07 +0200

    Correctly displaying weather forecast

--- app/src/main/java/com/oskarjerzyk/weatherapp/ForecastArrayAdapter.java
+++ app/src/main/java/com/oskarjerzyk/weatherapp/ForecastArrayAdapter.java
@@ -1,118 +1,76 @@
 package com.oskarjerzyk.weatherapp;
 
-import android.app.Activity;
+import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-public class ForecastArrayAdapter extends ArrayAdapter<Forecast> {
+public class ForecastArrayAdapter extends RecyclerView.Adapter<ForecastArrayAdapter.ForecastViewHolder> {
 
-    private ArrayList<Forecast> forecasts = new ArrayList<>();
+    Context context;
+    ArrayList<Forecast> data;
 
-    public ForecastArrayAdapter(Activity context, ArrayList<Forecast> forecasts) {
-        super(context, 0, forecasts);
+    public ForecastArrayAdapter(Context context, ArrayList<Forecast> data) {
+        this.context = context;
+        this.data = data;
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public ForecastViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View cardItemView = convertView;
-        if (cardItemView == null) {
-            cardItemView = LayoutInflater
-                    .from(getContext())
-                    .inflate(R.layout.forecast_card, parent, false);
-        }
+        View v;
+        v = LayoutInflater.from(context).inflate(R.layout.forecast_card, parent, false);
+        ForecastViewHolder viewHolder = new ForecastViewHolder(v);
 
-        Forecast currentForecast = getItem(position);
-        double temp = currentForecast.getTemp();
-        long time = currentForecast.getTime();
-        String icon = currentForecast.getIcon();
-        String desc = currentForecast.getDesc();
-
-        TextView temperatureTextView = (TextView) cardItemView.findViewById(R.id.tempetature_card_textview);
-        TextView timeTextView = (TextView) cardItemView.findViewById(R.id.hour_card_textview);
-        TextView dateTextView = (TextView) cardItemView.findViewById(R.id.date_card_textview);
-        TextView descTextView = (TextView) cardItemView.findViewById(R.id.description_card_textview);
-        ImageView iconImageView = (ImageView) cardItemView.findViewById(R.id.icon_card_imageview);
-
-        temperatureTextView.setText(Integer.toString(convertKelvinToCelsius(temp)));
-        timeTextView.setText(convertUnixTimeToHour(time));
-        dateTextView.setText(convertUnixTimeToDate(time));
-        descTextView.setText(desc);
-
-        switch (icon) {
-            case "01d":
-                iconImageView.setImageResource(R.drawable.d1);
-                break;
-            case "02d":
-                iconImageView.setImageResource(R.drawable.d2);
-                break;
-            case "03d":
-                iconImageView.setImageResource(R.drawable.d3);
-                break;
-            case "04d":
-                iconImageView.setImageResource(R.drawable.d4);
-                break;
-            case "09d":
-                iconImageView.setImageResource(R.drawable.d9);
-                break;
-            case "10d":
-                iconImageView.setImageResource(R.drawable.d10);
-                break;
-            case "11d":
-                iconImageView.setImageResource(R.drawable.d11);
-                break;
-            case "13d":
-                iconImageView.setImageResource(R.drawable.d13);
-                break;
-            case "50d":
-                iconImageView.setImageResource(R.drawable.d50);
-                break;
-            case "01n":
-                iconImageView.setImageResource(R.drawable.n1);
-                break;
-            case "02n":
-                iconImageView.setImageResource(R.drawable.n2);
-                break;
-            case "03n":
-                iconImageView.setImageResource(R.drawable.d3);
-                break;
-            case "04n":
-                iconImageView.setImageResource(R.drawable.d4);
-                break;
-            case "09n":
-                iconImageView.setImageResource(R.drawable.d9);
-                break;
-            case "10n":
-                iconImageView.setImageResource(R.drawable.n10);
-                break;
-            case "11n":
-                iconImageView.setImageResource(R.drawable.d11);
-                break;
-            case "13n":
-                iconImageView.setImageResource(R.drawable.d13);
-                break;
-            case "50n":
-                iconImageView.setImageResource(R.drawable.d50);
-                break;
-        }
-
-        return cardItemView;
+        return viewHolder;
     }
 
-    private String convertUnixTimeToDate(long t) {
-        t *= 1000;
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        return simpleDateFormat.format(t);
+    @Override
+    public void onBindViewHolder(@NonNull ForecastViewHolder holder, int position) {
+
+        holder.tempTextView.setText(Integer.toString(convertKelvinToCelsius(data.get(position).getTemp())) + (char) 0x00B0 + "C");
+        holder.hourTextView.setText(convertUnixTimeToHour(data.get(position).getTime()));
+        holder.dateTextView.setText(convertUnixTimeToDate(data.get(position).getTime()));
+        holder.descTextView.setText(data.get(position).getDesc());
+        holder.iconImageView.setImageResource(selectIconPath(data.get(position).getIcon()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public static class ForecastViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tempTextView;
+        private TextView hourTextView;
+        private TextView dateTextView;
+        private TextView descTextView;
+        private ImageView iconImageView;
+
+        public ForecastViewHolder(View itemView) {
+            super(itemView);
+
+            tempTextView = (TextView) itemView.findViewById(R.id.tempetature_card_textview);
+            hourTextView = (TextView) itemView.findViewById(R.id.hour_card_textview);
+            dateTextView = (TextView) itemView.findViewById(R.id.date_card_textview);
+            descTextView = (TextView) itemView.findViewById(R.id.description_card_textview);
+            iconImageView = (ImageView) itemView.findViewById(R.id.icon_card_imageview);
+        }
+    }
+
+    private int convertKelvinToCelsius(double k) {
+        k -= 273.15;
+        int c = (int) k;
+        return c;
     }
 
     private String convertUnixTimeToHour(long t) {
@@ -121,9 +79,51 @@ public class ForecastArrayAdapter extends ArrayAdapter<Forecast> {
         return simpleDateFormat.format(t);
     }
 
-    private int convertKelvinToCelsius(double k) {
-        k -= 273.15;
-        int c = (int) k;
-        return c;
+    private String convertUnixTimeToDate(long t) {
+        t *= 1000;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        return simpleDateFormat.format(t);
+    }
+
+    private int selectIconPath(String icon) {
+        switch (icon) {
+            case "01d":
+                return R.drawable.d1;
+            case "02d":
+                return R.drawable.d2;
+            case "03d":
+                return R.drawable.d3;
+            case "04d":
+                return R.drawable.d4;
+            case "09d":
+                return R.drawable.d9;
+            case "10d":
+                return R.drawable.d10;
+            case "11d":
+                return R.drawable.d11;
+            case "13d":
+                return R.drawable.d13;
+            case "50d":
+                return R.drawable.d50;
+            case "01n":
+                return R.drawable.n1;
+            case "02n":
+                return R.drawable.n2;
+            case "03n":
+                return R.drawable.d3;
+            case "04n":
+                return R.drawable.d4;
+            case "09n":
+                return R.drawable.d9;
+            case "10n":
+                return R.drawable.n10;
+            case "11n":
+                return R.drawable.d11;
+            case "13n":
+                return R.drawable.d13;
+            case "50n":
+                return R.drawable.d50;
+        }
+        return 0;
     }
 }
--- app/src/main/java/com/oskarjerzyk/weatherapp/ForecastFragment.java
+++ app/src/main/java/com/oskarjerzyk/weatherapp/ForecastFragment.java
@@ -1,10 +1,20 @@
 package com.oskarjerzyk.weatherapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,19 +26,94 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class ForecastFragment extends Fragment {
+public class ForecastFragment extends Fragment implements LocationListener {
+
+    private ArrayList<Forecast> weatherForecast;
+
+    private LocationManager locationManager;
+
+    private View view;
+    private RecyclerView recyclerView;
+    private ForecastArrayAdapter forecastArrayAdapter;
 
     private double lat;
     private double lon;
+
+    public ForecastFragment() {
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_forecast, container, false);
+        view = inflater.inflate(R.layout.fragment_forecast, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.forecast_recyclerview);
+        forecastArrayAdapter = new ForecastArrayAdapter(getContext(), weatherForecast);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(forecastArrayAdapter);
+
+        return view;
     }
 
-    private class ForecastAscyncTask extends AsyncTask<URL, Void, ArrayList<Forecast>> {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        weatherForecast = new ArrayList<>();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 60000, 0, this);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 0, this);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lon = location.getLongitude();
+
+        ForecastAsyncTask forecastAsyncTask = new ForecastAsyncTask();
+        forecastAsyncTask.execute();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    private class ForecastAsyncTask extends AsyncTask<URL, Void, ArrayList<Forecast>> {
 
         @Override
         protected ArrayList<Forecast> doInBackground(URL... urls) {
@@ -49,6 +134,14 @@ public class ForecastFragment extends Fragment {
                 Toast.makeText(getContext(), "JSON processing error!", Toast.LENGTH_SHORT).show();
             }
             return forecasts;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Forecast> forecasts) {
+
+            weatherForecast.clear();
+            weatherForecast.addAll(forecasts);
+            forecastArrayAdapter.notifyDataSetChanged();
         }
     }
 }
